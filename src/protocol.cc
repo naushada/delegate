14c5cac004a45d20fbcb26c059efbfa256eef0d1
naushada <naushad.dln@gmail.com>
2021-02-17 19:00:11 +0530

    http header updated

--- src/protocol.cc
+++ src/protocol.cc
@@ -914,10 +914,9 @@ int32_t mna::ddns::client::buildWanIPRequest(std::string& req)
   req += "Host: ip1.dynupdate.no-ip.com\r\n";
   req += "Connection: keep-alive\r\n";
   req += "Content-Length: 0\r\n";
-  req += "User-Agent: Balaagh ilm/Ubuntu balaagh.technologies@gmail.com\r\n";
-  req += "Accept: \r\n";
-  req += "Accept-Encoding: \r\n";
-  req += "Accept-Language: \r\n";
+  req += "User-Agent: curl/7.16.3 libcurl/7.16.3 OpenSSL/0.9.7l zlib/1.2.3\r\n";
+  req += "Accept-Encoding: *\r\n";
+  req += "Accept-Language: en, mi\r\n";
   req += "\r\n\r\n";
 
   return(0);
@@ -946,10 +945,9 @@ int32_t mna::ddns::client::buildWanIPUpdateRequest(std::string& req, vddnsPeer& 
   req += " \r\n";
   req += "Connection: keep-alive\r\n";
   req += "Content-Length: 0\r\n";
-  req += "User-Agent: Balaagh ilm/ubuntu balaagh.technologies@gmail.com\r\n";
-  req += "Accept: \r\n";
-  req += "Accept-Encoding: \r\n";
-  req += "Accept-Language: \r\n";
+  req += "User-Agent: curl/7.16.3 libcurl/7.16.3 OpenSSL/0.9.7l zlib/1.2.3\r\n";
+  req += "Accept-Encoding: *\r\n";
+  req += "Accept-Language: en, mi\r\n";
   req += "\r\n\r\n";
 
   return(0);
